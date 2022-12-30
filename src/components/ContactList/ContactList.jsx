de74f1844b94587301f5e80b84fa3704837f9679
Ihor Smyk <ismyk256@gmail.com>
2022-12-30 17:00:05 +0200

    upd

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,15 +1,17 @@
 // import PropTypes from 'prop-types';
-import s from './Contact.module.css';
+import s from './ContactList.module.css';
 
 const ContactList = ({ contacts, onDeleteContact }) => {
   return (
-    <ul className={ s.contactList}>
-      {contacts.map({id,name,number} => { 
-        <li></li>
+    <ul className={s.contactList}>
+      {contacts.map(({ id, name, number }) => {
+        return (
+          <li key={id}>
+            {name} <br /> {number}
+          </li>
+        );
       })}
-
-</ul>
-
+    </ul>
   );
 };
 

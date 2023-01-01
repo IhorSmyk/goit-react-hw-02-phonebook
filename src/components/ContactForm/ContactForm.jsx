d0098643cf4e2fd29031e4cf8bc1b91e3fad8916
Ihor Smyk <ismyk256@gmail.com>
2023-01-01 22:20:13 +0200

    upd

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,5 +1,5 @@
 import { Component } from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import s from './ContactForm.module.css';
 
 class InputForm extends Component {
@@ -8,18 +8,29 @@ class InputForm extends Component {
     number: '',
   };
 
+  handleChangeName = e => {
+    this.setState({ name: e.target.value });
+  };
+
+  handleChangeNumber = e => {
+    this.setState({ number: e.target.value });
+  };
+
   render() {
     return (
       <>
-        <form action="submit" onSubmit={0}>
+        <form className={s.form} action="submit" onSubmit={0}>
           <label htmlFor="name">Name</label>
           <input
             id="name"
             type="text"
             name="name"
+            placeholder="Enter a name"
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
+            onChange={null}
+            value={this.state.name}
           />
 
           <label htmlFor="number">Number</label>
@@ -27,9 +38,12 @@ class InputForm extends Component {
             id="number"
             type="text"
             name="number"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            placeholder="Enter a number"
+            pattern=" ^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$"
+            title="Name may contain numbers"
             required
+            onChange={null}
+            value={this.state.number}
           />
           <button type="submit">Add contact to the list</button>
         </form>
